475403250881b3d31784053217b5c450e64f3b6f
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-02-28 20:29:36 +0200

    make build

---
+++ index.62b3248c.js
@@ -0,0 +1,3 @@
+/* global alert */document.querySelector("#lang").addEventListener("click",function(){alert("feature under development")});//# sourceMappingURL=index.62b3248c.js.map
+
+//# sourceMappingURL=index.62b3248c.js.map
